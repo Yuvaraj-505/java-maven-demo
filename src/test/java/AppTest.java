46ce3c29b92a36a9d467e5411974c09e177165db
Dhanush Kumar <dk652003@gmail.com>
2026-08-20 11:57:34 +0530

    Test code written

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,17 @@
+
+    import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+    public class AppTest {
+
+        @Test
+        void testGreeting() {
+
+            assertEquals(2, 1 + 1);
+
+        }
+
+    }
+
+    
